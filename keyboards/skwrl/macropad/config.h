731dafb10ae30591ac2d46440dabcb43c0871d74
yuvachang <yuva.chang@gmail.com>
2024-08-07 00:53:46 -0600

    add skwrl keyboards; macropad files

---
+++ keyboards/skwrl/macropad/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define ENCODERS_PAD_A { D7 } //Pro Micro pin on Out A
+#define ENCODERS_PAD_B { E6 } //Pro Micro pin on Out B
+#define ENCODER_RESOLUTIONS { 4 } //Pulses the encoder registers between each detent
---
+++ keyboards/skwrl/macropad/keymaps/via/keymap.c
@@ -0,0 +1,17 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_F14,   KC_KB_MUTE,
+        KC_F18,   KC_F20
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
+};
+#endif
